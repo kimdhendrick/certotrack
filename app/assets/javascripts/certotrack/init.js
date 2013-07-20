8ec8ad09b3a81fecd819f25c7918db0b6acbccbc
Kim D Barnes <kimdbarnes@gmail.com>
2013-07-20 14:33:20 -0600

    Fixed multiple start of BackBone router error

--- app/assets/javascripts/certotrack/init.js
+++ app/assets/javascripts/certotrack/init.js
@@ -1,7 +1,1 @@
 window.Certotrack || (window.Certotrack = {});
-
-$(document).ready(function () {
-  console.log('starting router');
-  new Certotrack.Router();
-  Backbone.history.start();
-});
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Certotrack::Application.routes.draw do
 
   resources :sessions, only: [:create, :destroy, :signed_in]
   match '/signed_in', to: 'sessions#signed_in'
-
-  match '/login', to: 'sessions#new', :via => [:get]
-
-  #do I need :delete?
   match '/logout', to: 'sessions#destroy', :via => [:get, :delete]
+  match '/login', to: 'certotrack#welcome', :via => [:get]
 end
