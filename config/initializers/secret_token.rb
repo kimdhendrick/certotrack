923983f03c6b99d8265aea22ecebc7ff1e6b5f2d
Kim D Barnes <kimdbarnes@gmail.com>
2013-07-31 21:44:38 -0600

    Rails 4 said to do this.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Certotrack::Application.config.secret_token = '253a7b8200d78a92563f3a8dd7a2fc9877ba9793f8d0a2f113eba79493795224567b1a4126da9f04a1bf0b601adeb9147c590af5f10685cc93948935926b44fc'
+Certotrack::Application.config.secret_key_base = "canjustanythinggohere"
