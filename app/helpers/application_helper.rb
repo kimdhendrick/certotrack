0bf11d5b9fd359d12070b885406daa9ba0454742
Kim D Barnes <kimdbarnes@gmail.com>
2013-08-22 09:53:20 -0600

    Sorted columns show current sort direction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
-    direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
-    link_to title, :sort => column, :direction => direction
+    direction = _sort_direction(column)
+    css_class = (column == params[:sort]) ? "current #{direction}" : nil
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
+
+  def _sort_direction(column)
+    (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
   end
 end
