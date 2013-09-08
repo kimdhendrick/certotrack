4fa1a04c46456b4bdb9ed6ba8739c6f5f9608b38
Kim D Barnes <kimdbarnes@gmail.com>
2013-09-08 13:33:47 -0600

    Location name should be unique regardless of case or spaces

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,11 @@
 class Location < ActiveRecord::Base
 
   belongs_to :customer
+
+  before_validation :_strip_whitespace
+
   validates_presence_of :customer
+  validates_uniqueness_of :name, scope: :customer_id, case_sensitive: false
 
   def self.accessible_parameters
     [:name]
@@ -9,6 +13,12 @@ class Location < ActiveRecord::Base
 
   def to_s
     name
+  end
+
+  private
+
+  def _strip_whitespace
+    self.name.try(:strip!)
   end
 
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -7,6 +7,49 @@ describe Location do
 
   it { should belong_to(:customer) }
   it { should validate_presence_of :customer }
+  it { should validate_uniqueness_of(:name).scoped_to(:customer_id) }
+
+  describe 'uniqueness of name' do
+    subject { create(:location, name: 'cat', customer: customer) }
+    let(:customer) { create(:customer) }
+
+    before do
+      subject.valid?
+    end
+
+    it 'should not allow duplicate names when exact match' do
+      copycat = Location.new(name: 'cat', customer: customer)
+      copycat.should_not be_valid
+      copycat.errors.full_messages_for(:name).should == ['Name has already been taken']
+    end
+
+    it 'should not allow duplicate names when differ by case' do
+      copycat = Location.new(name: 'CAt', customer: customer)
+      copycat.should_not be_valid
+      copycat.errors.full_messages_for(:name).should == ['Name has already been taken']
+    end
+
+    it 'should not allow duplicate names when differ by leading space' do
+      copycat = Location.new(name: ' cat', customer: customer)
+      copycat.should_not be_valid
+      copycat.errors.full_messages_for(:name).should == ['Name has already been taken']
+    end
+
+    it 'should not allow duplicate names when differ by trailing space' do
+      copycat = Location.new(name: 'cat ', customer: customer)
+      copycat.should_not be_valid
+      copycat.errors.full_messages_for(:name).should == ['Name has already been taken']
+    end
+  end
+
+  describe 'whitespace stripping' do
+    it 'should strip trailing and leading whitespace' do
+      customer = create(:customer)
+      cat = create(:location, name: ' cat ', customer: customer)
+      cat.reload
+      cat.name.should == 'cat'
+    end
+  end
 
   it 'should display its name as to_s' do
     @location.name = 'My Location'
