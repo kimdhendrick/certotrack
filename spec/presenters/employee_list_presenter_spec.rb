a1b2691515f7bd78489294a64ea9c2cb2934b547
Kim D Barnes <kimdbarnes@gmail.com>
2013-09-29 16:22:44 -0600

    More tests for EmployeeListPresenter

--- spec/presenters/employee_list_presenter_spec.rb
+++ spec/presenters/employee_list_presenter_spec.rb
@@ -1,40 +1,47 @@
 require 'spec_helper'
 
 describe EmployeeListPresenter do
-  describe '#initialize' do
-    it 'should wrap models in EmployeePresenters' do
-      employee = build(:employee)
-      fake_sorter = Faker.new()
-
-      sut = EmployeeListPresenter.new([employee], {sorter: fake_sorter})
-
-      sut.collection.map(&:model).should == [employee]
-    end
-  end
-
   describe '#present' do
     it 'should sort' do
       employee = build(:employee)
       fake_sorter = Faker.new([EmployeePresenter.new(employee)])
 
-      sut = EmployeeListPresenter.new([employee], {sorter: fake_sorter})
+      presenter = EmployeeListPresenter.new([employee], {sorter: fake_sorter})
 
-      sut.present
+      presenter.present({sort: :field, direction: :asc})
 
       fake_sorter.received_message.should == :sort
       fake_sorter.received_params[0][0].model.should == employee
+      fake_sorter.received_params[1].should == :field
+      fake_sorter.received_params[2].should == :asc
     end
 
     it 'should paginate' do
       employee = build(:employee)
-      fake_paginator = Faker.new()
+      fake_paginator = Faker.new
 
-      sut = EmployeeListPresenter.new([employee], {paginator: fake_paginator})
+      presenter = EmployeeListPresenter.new([employee], {paginator: fake_paginator})
 
-      sut.present
+      presenter.present(page: 100)
 
       fake_paginator.received_message.should == :paginate
       fake_paginator.received_params[0][0].model.should == employee
+      fake_paginator.received_params[1].should == 100
+    end
+    
+    it 'should return a collection of EmployeePresenters' do
+      employee = build(:employee)
+
+      results = EmployeeListPresenter.new([employee]).present
+
+      results.map(&:model).should == [employee]
+      results.first.should be_an_instance_of(EmployeePresenter)
+    end
+
+    it 'should handle empty array' do
+      presenter = EmployeeListPresenter.new([])
+
+      presenter.present.count.should == 0
     end
   end
 end
