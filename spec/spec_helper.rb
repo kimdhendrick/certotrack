784449f65b9b5f4c18ffaa59d24c2aba7cf0639e
Kim D Barnes <kimdbarnes@gmail.com>
2013-10-09 20:50:22 -0600

    Refactor: moved shared examples to common directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,12 +9,11 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
 require 'support/faker'
-require 'support/shared_examples/whitespace_stripping'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/shared_examples/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.include LoginHelpers
