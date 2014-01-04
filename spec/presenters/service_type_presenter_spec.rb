9774dfd8f1d530c58b9b450a238c95994447642b
Kim D Barnes <kimdbarnes@gmail.com>
2014-01-04 09:43:16 -0700

    Refactor test

--- spec/presenters/service_type_presenter_spec.rb
+++ spec/presenters/service_type_presenter_spec.rb
@@ -6,49 +6,47 @@ describe ServiceTypePresenter do
     build(:service_type,
           name: 'Oil Change',
           expiration_type: ServiceType::EXPIRATION_TYPE_BY_DATE,
-          interval_date: Interval::ONE_MONTH.text,
+          interval_date: Interval::THREE_MONTHS.text,
           interval_mileage: 5000,
           customer: create(:customer, name: 'myCustomer'))
   end
 
-  subject { ServiceTypePresenter.new(create(:service_type)) }
+  subject { ServiceTypePresenter.new(service_type) }
 
   it 'should respond to model' do
-    ServiceTypePresenter.new(service_type).model.should == service_type
+    subject.model.should == service_type
   end
 
   it 'should respond to id' do
-    ServiceTypePresenter.new(service_type).id.should == service_type.id
+    subject.id.should == service_type.id
   end
 
   it 'should respond to name' do
-    ServiceTypePresenter.new(service_type).id.should == service_type.id
+    subject.id.should == service_type.id
   end
 
   it 'should respond to expiration_type' do
-    ServiceTypePresenter.new(service_type).expiration_type.should == 'By Date'
+    subject.expiration_type.should == 'By Date'
   end
 
   it 'should respond to interval_date' do
-    ServiceTypePresenter.new(service_type).interval_date.should == Interval::ONE_MONTH.text
+    subject.interval_date.should == Interval::THREE_MONTHS.text
   end
 
   it 'should respond to interval_mileage' do
-    ServiceTypePresenter.new(service_type).interval_mileage.should == '5,000'
+    subject.interval_mileage.should == '5,000'
   end
 
   it 'should respond to sortable_interval_mileage' do
-    ServiceTypePresenter.new(service_type).sortable_interval_mileage.should == 5000
+    subject.sortable_interval_mileage.should == 5000
   end
 
   it 'should respond to sort_key' do
-    ServiceTypePresenter.new(service_type).sort_key.should == 'Oil Change'
+    subject.sort_key.should == 'Oil Change'
   end
 
   it 'should respond to interval_date_code' do
-    service_type = create(:service_type, interval_date: Interval::THREE_MONTHS.text)
-
-    ServiceTypePresenter.new(service_type).interval_date_code.should == Interval::THREE_MONTHS.id
+    subject.interval_date_code.should == Interval::THREE_MONTHS.id
   end
 
   it 'should respond to expiration_type_of_date?' do
