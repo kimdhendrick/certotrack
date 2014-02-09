e1b0c4ee097cc350acda16cadbed49a07701cacd
Kim Barnes & Meeka Gayhart <pair+kim+meeka@certotrack>
2014-02-09 13:58:51 -0700

    Reservice vehicle prompts for future dates
    [Finishes #64971052]

--- spec/features/reservice_vehicle_spec.rb
+++ spec/features/reservice_vehicle_spec.rb
@@ -8,13 +8,13 @@ describe 'Reservice Vehicle Pages' do
   let(:vehicle) do
     create(:vehicle, license_plate: '123-ABC', vehicle_number: '34987', year: 1999, vehicle_model: 'Dart')
   end
-  let(:service_type) { create(:service_type, name: 'Oil change')}
+  let(:service_type) { create(:service_type, name: 'Oil change') }
   let(:service_period) do
     build(
-        :service_period,
-        start_mileage: 10001,
-        start_date: Date.new(2013, 5, 16),
-        comments: 'Best oil change evah!')
+      :service_period,
+      start_mileage: 10001,
+      start_date: Date.new(2013, 5, 16),
+      comments: 'Best oil change evah!')
   end
   let(:service) { create(:service, customer: customer, vehicle: vehicle, service_type: service_type) }
 
@@ -44,13 +44,13 @@ describe 'Reservice Vehicle Pages' do
       it { should have_link('Home', root_path) }
       it { should have_link('Create Service', href: new_service_path) }
       it { should have_selector('span.label', text: 'Vehicle') }
-      it { should have_selector('span.value', text: '123-ABC/34987 1999 Dart')}
+      it { should have_selector('span.value', text: '123-ABC/34987 1999 Dart') }
       it { should have_selector('span.label', text: 'Service Type') }
       it { should have_selector('span.value', text: 'Oil change') }
       it { should have_selector('span.value', text: 'Oil change') }
-      specify { find_field('Last Service Mileage').value.should == '10001'}
-      specify { find_field('Last Service Date').value.should == '05/16/2013'}
-      specify { find_field('Comments').value.should == 'Best oil change evah!'}
+      specify { find_field('Last Service Mileage').value.should == '10001' }
+      specify { find_field('Last Service Date').value.should == '05/16/2013' }
+      specify { find_field('Comments').value.should == 'Best oil change evah!' }
     end
 
     describe 'with invalid information' do
@@ -76,6 +76,31 @@ describe 'Reservice Vehicle Pages' do
         before { click_button('Reservice') }
 
         it { should have_selector('h1', text: 'Show Service') }
+      end
+    end
+
+    describe 'future last service date' do
+      before do
+        fill_in 'Last Service Mileage', with: 10000
+        fill_in 'Last Service Date', with: '01/22/2055'
+      end
+
+      it 'should prompt user', js: true do
+        click_on 'Reservice'
+
+        alert = page.driver.browser.switch_to.alert
+        alert.text.should eq('Are you sure you want to enter a future date?')
+        alert.dismiss
+
+        page.should have_content 'Reservice Vehicle'
+
+        click_on 'Reservice'
+
+        alert = page.driver.browser.switch_to.alert
+        alert.text.should eq('Are you sure you want to enter a future date?')
+        alert.accept
+
+        page.should have_content 'Show Service'
       end
     end
   end
