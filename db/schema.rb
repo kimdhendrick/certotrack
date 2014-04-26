ea9a1d6b59317931e294fce9c891ccd30ae9a671
Kim D Barnes <kimdbarnes@gmail.com>
2014-04-26 09:17:39 -0600

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140322144548) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "certification_periods", force: true do |t|
     t.string   "trainer"
     t.datetime "start_date"
