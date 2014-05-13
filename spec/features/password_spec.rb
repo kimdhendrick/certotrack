5409f0b5ad8a5c9308f77be77d8cd9080da524e2
Kim D Barnes <kimdbarnes@gmail.com>
2014-05-12 20:03:10 -0600

    Password pages styling
    [#55343906]

--- spec/features/password_spec.rb
+++ spec/features/password_spec.rb
@@ -13,25 +13,25 @@ describe 'Password Security', slow: true do
       fill_in 'Current Password', with: 'PasswordBAD'
       fill_in 'New Password', with: 'password123'
       fill_in 'Confirm New Password', with: 'password123'
-      click_on 'Change Password'
+      click_on 'Update'
       page.should have_content 'Current password is invalid'
 
       fill_in 'Current Password', with: 'Password123'
       fill_in 'New Password', with: 'password123'
       fill_in 'Confirm New Password', with: 'password123'
-      click_on 'Change Password'
+      click_on 'Update'
       page.should have_content 'Password must be at least 8 characters long and must contain at least one digit and combination of upper and lower case.'
 
       fill_in 'Current Password', with: 'Password123'
       fill_in 'New Password', with: 'Password555'
       fill_in 'Confirm New Password', with: 'Password666'
-      click_on 'Change Password'
+      click_on 'Update'
       page.should have_content "Password confirmation doesn't match Password"
 
       fill_in 'Current Password', with: 'Password123'
       fill_in 'New Password', with: 'Password321'
       fill_in 'Confirm New Password', with: 'Password321'
-      click_on 'Change Password'
+      click_on 'Update'
 
       page.should have_content 'Password updated successfully.'
       page.should have_content 'Welcome'
@@ -47,27 +47,27 @@ describe 'Password Security', slow: true do
       login_as(user)
 
       page.should have_field 'password'
-      page.should have_content 'Renew your password'
-      page.should have_button 'Change my password'
+      page.should have_content 'Password Expired'
+      page.should have_button 'Update'
 
       page.fill_in 'Current password', with: 'Password123'
       page.fill_in 'New password', with: 'p'
       page.fill_in 'Confirm new password', with: 'p'
-      click_on 'Change my password'
+      click_on 'Update'
 
       page.should have_content 'Password must be at least 8 characters long and must contain at least one digit and combination of upper and lower case.'
 
       page.fill_in 'Current password', with: 'Password123'
       page.fill_in 'New password', with: 'password123'
       page.fill_in 'Confirm new password', with: 'password123'
-      click_on 'Change my password'
+      click_on 'Update'
 
       page.should have_content 'Password must be at least 8 characters long and must contain at least one digit and combination of upper and lower case.'
 
       page.fill_in 'Current password', with: 'Password123'
       page.fill_in 'New password', with: 'Password123'
       page.fill_in 'Confirm new password', with: 'Password123'
-      click_on 'Change my password'
+      click_on 'Update'
 
       page.should have_content 'Password may not be the same as current password.'
       page.should have_content 'Password may not be one used recently.'
@@ -75,7 +75,7 @@ describe 'Password Security', slow: true do
       page.fill_in 'Current password', with: 'Password123'
       page.fill_in 'New password', with: 'Password1234'
       page.fill_in 'Confirm new password', with: 'Password1234'
-      click_on 'Change my password'
+      click_on 'Update'
 
       page.should have_content 'Welcome'
     end
